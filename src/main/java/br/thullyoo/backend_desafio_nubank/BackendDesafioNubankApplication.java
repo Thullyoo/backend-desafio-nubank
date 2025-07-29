4d2922c615229af1730f6d0f41c810305377d296
Thullyoo <thullyofilho@gmail.com>
2025-07-29 12:47:29 -0300

    Initial commit

---
+++ src/main/java/br/thullyoo/backend_desafio_nubank/BackendDesafioNubankApplication.java
@@ -0,0 +1,13 @@
+package br.thullyoo.backend_desafio_nubank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDesafioNubankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDesafioNubankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/thullyoo/backend_desafio_nubank/BackendDesafioNubankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.thullyoo.backend_desafio_nubank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDesafioNubankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

da1d38589cb0cb4a591808a719e00b2e5db1fa78
Thullyoo <thullyofilho@gmail.com>
2025-08-01 12:23:35 -0300

    feat: add Client model

---
+++ src/main/java/br/thullyoo/backend_desafio_nubank/model/Client.java
@@ -0,0 +1,33 @@
+package br.thullyoo.backend_desafio_nubank.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Table(name = "TB_CLIENTS")
+@Entity
+@Data
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String document;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "client")
+    private List<Contact> contacts;
+}

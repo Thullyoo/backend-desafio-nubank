811029b6832f884c4d90eaf2fff4d6e1381721c8
Thullyoo <thullyofilho@gmail.com>
2025-08-11 11:41:34 -0300

    feat: add endpoint to list and listcontact

--- src/main/java/br/thullyoo/backend_desafio_nubank/controller/ClientController.java
+++ src/main/java/br/thullyoo/backend_desafio_nubank/controller/ClientController.java
@@ -2,24 +2,39 @@ package br.thullyoo.backend_desafio_nubank.controller;
 
 import br.thullyoo.backend_desafio_nubank.dtos.ClientRequest;
 import br.thullyoo.backend_desafio_nubank.dtos.ClientResponse;
+import br.thullyoo.backend_desafio_nubank.dtos.ContactResponse;
+import br.thullyoo.backend_desafio_nubank.model.Contact;
 import br.thullyoo.backend_desafio_nubank.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/clients")
 public class ClientController {
 
     @Autowired
     private ClientService clientService;
 
-    @PostMapping("/clients")
+    @PostMapping
     public ResponseEntity<ClientResponse> registerClient(@RequestBody ClientRequest clientRequest){
         ClientResponse clientResponse = clientService.register(clientRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(clientResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClientResponse>> listClients(){
+        List<ClientResponse> clientResponse = clientService.list();
+        return ResponseEntity.status(HttpStatus.OK).body(clientResponse);
+    }
+
+    @GetMapping("${id}/contacts")
+    public ResponseEntity<List<ContactResponse>> listContactByClient(@PathVariable("id") Long client_id){
+        List<ContactResponse> contactResponses =  clientService.listContact(client_id);
+        return ResponseEntity.status(HttpStatus.OK).body(contactResponses);
     }
 
 }

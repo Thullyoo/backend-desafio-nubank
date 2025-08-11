38f47408dc6e6caf2f8b110f18dca286390a61d8
Thullyoo <thullyofilho@gmail.com>
2025-08-11 11:41:16 -0300

    feat: add method to list and listcontact on ClientService

--- src/main/java/br/thullyoo/backend_desafio_nubank/services/ClientService.java
+++ src/main/java/br/thullyoo/backend_desafio_nubank/services/ClientService.java
@@ -2,11 +2,19 @@ package br.thullyoo.backend_desafio_nubank.services;
 
 import br.thullyoo.backend_desafio_nubank.dtos.ClientRequest;
 import br.thullyoo.backend_desafio_nubank.dtos.ClientResponse;
+import br.thullyoo.backend_desafio_nubank.dtos.ContactResponse;
 import br.thullyoo.backend_desafio_nubank.mapper.ClientMapper;
+import br.thullyoo.backend_desafio_nubank.mapper.ContactMapper;
 import br.thullyoo.backend_desafio_nubank.model.Client;
+import br.thullyoo.backend_desafio_nubank.model.Contact;
 import br.thullyoo.backend_desafio_nubank.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClientService {
@@ -22,5 +30,37 @@ public class ClientService {
 
         return ClientMapper.clientToClientResponse(client);
 
+    }
+
+    public List<ClientResponse> list(){
+        List<Client> clients = clientRepository.findAll();
+        List<ClientResponse> clientResponses = new ArrayList<>();
+
+        for(Client client : clients){
+            clientResponses.add(ClientMapper.clientToClientResponse(client));
+        }
+
+        return clientResponses;
+    }
+
+    public List<ContactResponse> listContact(Long client_id){
+
+        Optional<Client> client =  clientRepository.findById(client_id);
+
+        if (client.isEmpty()){
+            throw new RuntimeException("Client not exist");
+        }
+
+        if (client.get().getContacts().isEmpty()){
+            return List.of();
+        }
+
+        List<ContactResponse> contacts = new ArrayList<>();
+
+        for (Contact contact : client.get().getContacts()){
+            contacts.add(ContactMapper.contactToContactResponse(contact));
+        }
+
+        return contacts;
     }
 }
